b89ca63b803e53339a7cc431e87cbededee6cc82
TheRandomLabs <therandomlabsinc@gmail.com>
2019-11-13 11:32:54 +1100

    Fixed scripts

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,9 +1,9 @@
-param([String]$upstream = "ScoopInstaller/Main:master")
+param(
+    # overwrite upstream param
+    [String]$upstream = "lukesampson/scoop-extras:master"
+)
 
-if (!$env:SCOOP_HOME) {
-    $env:SCOOP_HOME = Resolve-Path(Split-Path(Split-Path(scoop which scoop)))
-}
-
-$auto_pr = "$env:SCOOP_HOME\bin\auto-pr.ps1"
-$dir = "$psscriptroot\..\bucket"
-Invoke-Expression -Command "& '$auto_pr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
+$autopr = "$env:SCOOP_HOME/bin/auto-pr.ps1"
+$dir = "$psscriptroot/../bucket" # checks the parent dir
+Invoke-Expression -command "& '$autopr' -dir '$dir' -upstream $upstream $($args | ForEach-Object { "$_ " })"
--- bin/test.ps1
+++ bin/test.ps1
@@ -1,7 +1,2 @@
-#requires -Modules @{ ModuleName = 'Pester'; ModuleVersion = '4.4.0' }
-
-if (!$env:SCOOP_HOME) {
-    $env:SCOOP_HOME = Resolve-Path(Split-Path(Split-Path(scoop which scoop))) 
-}
-
-Invoke-Pester "$psscriptroot\.."
+if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = resolve-path (split-path (split-path (scoop which scoop))) }
+Invoke-Pester "$psscriptroot/.."
